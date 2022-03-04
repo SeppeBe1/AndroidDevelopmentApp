509661d16277786f08ed7c2dd0c93d1fe416cb51
SeppeBe1 <seppe.bellens@hotmail.nl>
2022-03-04 15:40:08 +0100

    bug fix NullPointerExceptions

--- Recept/app/src/main/java/com/example/recept/MainActivity.java
+++ Recept/app/src/main/java/com/example/recept/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.recept;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,53 +20,46 @@ public class MainActivity extends AppCompatActivity {
     Integer melk = 25;
 
     Button minBtn;
+    Button plusBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView personsTv = findViewById(R.id.persons);
+        personsTv = findViewById(R.id.persons);
 
-        TextView ing1Tv = findViewById(R.id.ingedient1);
-        TextView ing2Tv= findViewById(R.id.ingedient2);
-        TextView ing3Tv = findViewById(R.id.ingedient3);
+        ing1Tv = findViewById(R.id.ingredient1);
+        ing2Tv= findViewById(R.id.ingredient2);
+        ing3Tv = findViewById(R.id.ingredient3);
 
-
-        Button minBtn = findViewById(R.id.minusBtn);
-        Button plusBtn = findViewById(R.id.plusBtn);
+        minBtn = findViewById(R.id.minusBtn);
+        plusBtn = findViewById(R.id.plusBtn);
         update();
 
-        minBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                persons-=2;
-                ei--;
-                bloem-=250;
-                melk-=25;
-                update();
+        minBtn.setOnClickListener(view -> {
+            persons-=2;
+            ei--;
+            bloem-=250;
+            melk-=25;
+            update();
 
-            }
         });
 
-        plusBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                persons+=2;
-                ei++;
-                bloem+=250;
-                melk+=25;
-                update();
-
-            }
+        plusBtn.setOnClickListener(view -> {
+            persons+=2;
+            ei++;
+            bloem+=250;
+            melk+=25;
+            update();
         });
-
     }
+
     void update(){
         personsTv.setText(String.format("Recept voor %d personen", persons));
-        ing1Tv.setText(String.format("Eieren %d personen", ei));
-        ing2Tv.setText(String.format("Eieren %d personen", bloem));
-        ing3Tv.setText(String.format("Eieren %d personen", melk));
+        ing1Tv.setText(String.format("Eieren %d ", ei));
+        ing2Tv.setText(String.format(" bloem %dg ", bloem));
+        ing3Tv.setText(String.format(" %ddl ", melk));
 
         minBtn.setEnabled(persons > 0);
     }
