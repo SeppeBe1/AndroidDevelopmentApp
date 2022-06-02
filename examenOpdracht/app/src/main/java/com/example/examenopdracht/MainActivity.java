898a557631f242f462c71d3915e53cab4f494725
SeppeBe1 <seppe.bellens@hotmail.nl>
2022-06-02 15:37:37 +0200

    creating and recieving responses from API

--- examenOpdracht/app/src/main/java/com/example/examenopdracht/MainActivity.java
+++ examenOpdracht/app/src/main/java/com/example/examenopdracht/MainActivity.java
@@ -1,14 +1,94 @@
 package com.example.examenopdracht;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rRecyclerView;
+    private ReceptAdapter rReceptAdapter;
+    private ArrayList<Recept> rReceptList;
+    private RequestQueue rRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rRecyclerView = findViewById(R.id.recyclerView);
+        rRecyclerView.setHasFixedSize(true);
+        rRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        rReceptList = new ArrayList<>();
+        rRequestQueue = Volley.newRequestQueue(this);
+        parseJSON();
+    }
+    private void parseJSON(){
+
+        String host = "https://api.spoonacular.com/recipes/";
+        String what = "complexSearch?";
+        String key = "apiKey=f370281a5a7947a08019c2134d25b356";
+
+        String number = "&number=2";
+        String inputsearch = "chicken";
+        String search = "&query=" + inputsearch;
+        String params = number + search;
+
+        String url = host + what + key + params;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new
+                Response.Listener<JSONObject>() {
+                    //Responsen aanvragen met GET en de url
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            for(int i = 0; i <jsonArray.length(); i++){
+                                JSONObject result = jsonArray.getJSONObject(i);
+
+                                String receptId = result.getString("id");
+                                String receptTitle = result.getString("title");
+                                String imageUrl = result.getString("image");
+                                //Responsen opvangen en uithalen wat nodig
+
+                                rReceptList.add(new Recept(receptId,receptTitle,imageUrl));
+                                //Responsen in Recept object steken
+                            }
+
+                            rReceptAdapter = new ReceptAdapter(MainActivity.this,rReceptList);
+                            rRecyclerView.setAdapter(rReceptAdapter);
+                            //zorgen er voor dat data in recyclerview terechtkomt
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+
+            }
+        });
+
+        rRequestQueue.add(request);
+
     }
 }
