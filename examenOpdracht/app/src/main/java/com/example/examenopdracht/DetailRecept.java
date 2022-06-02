5523429e27b415a95c1405cfdd6bd3d261b26e86
SeppeBe1 <seppe.bellens@hotmail.nl>
2022-06-02 20:47:37 +0200

    create onclicklistener on viewHolder

---
+++ examenOpdracht/app/src/main/java/com/example/examenopdracht/DetailRecept.java
@@ -0,0 +1,14 @@
+package com.example.examenopdracht;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailRecept extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_recept);
+    }
+}
